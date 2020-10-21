e163ad52d67887d5407bb764f878dadb8d01e58f
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-21 21:05:06 +0530

    [Chetan] Add. Solving Using Switch case.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -20,56 +20,29 @@ namespace EmployeeWage
             //Constants 
             const int IS_EMPLOYEE_FULL_TIME = 1;
             const int IS_EMPLOYEE_PART_TIME = 2;
-            
-            //Checks if employee is present, if present gives employee wage
-            //message, if employee not present displays absent
-            if (EmployeePresent() == IS_EMPLOYEE_FULL_TIME)
-            {
-                FullTimeEmployeeWage();
-            }
-            else
-            if (EmployeePresent() == IS_EMPLOYEE_PART_TIME)
-            {
-                PartTimeEmployeeWage();
-            }
-            else
-            {
-                Console.WriteLine("Employee was absent");
-            }
-        }
-        /// <summary>
-        /// Method to checks whether employee is  present or not
-        /// </summary>
-        public static int EmployeePresent()
-        {
+            const int EMP_RATE_PER_HOUR = 20;
+            //Variable
+            int employeeHours = 0;
+
             //Create reference of random class
             Random random = new Random();
             //Returns random value
-            return random.Next(0, 2);
-        }
-        /// <summary>
-        /// Method to calculate Full time employee wage
-        /// </summary>
-        public static int FullTimeEmployeeWage()
-        {
-            int EMP_RATE_PER_HOUR = 20;
-            int employeeHours = 8;
-            //calculates wage for full time employee
-            int fullTimeWage = employeeHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage is " + fullTimeWage);
-            return fullTimeWage;
-        }
-        /// <summary>
-        /// Method to calculate Part time employee wage
-        /// </summary>
-        public static int PartTimeEmployeeWage()
-        {
-            int EMP_RATE_PER_HOUR = 20;
-            int employeeHours = 4;
-            //calculates wage for part time employee
-            int partTimeWage = employeeHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage is " + partTimeWage);
-            return partTimeWage;            
-        }
+            int employeeCheck = random.Next(0, 2);
+
+            switch (employeeCheck) 
+            {
+                case IS_EMPLOYEE_FULL_TIME:
+                    employeeHours = 8;
+                    break;
+                case IS_EMPLOYEE_PART_TIME:
+                    employeeHours = 4;
+                    break;
+                default:
+                    employeeHours = 0;
+                    break;
+            }
+            int totalEmployeeWage = employeeHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Daily wage: " + totalEmployeeWage);
+        }        
     }
 }
