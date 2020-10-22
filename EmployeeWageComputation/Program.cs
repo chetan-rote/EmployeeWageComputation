b24af188c20314279150b9ac478085a42041c708
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-22 14:52:37 +0530

    [Chetan]Refactor. Employee wage code using class & method

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -13,27 +13,32 @@ namespace EmployeeWage
 {
     class Program
     {
+        //Constants 
+        const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_WORKING_HOURS_IN_MONTH = 100;
         static void Main(string[] args)
         {
-            //Constants 
-            const int IS_EMPLOYEE_FULL_TIME = 1;
-            const int IS_EMPLOYEE_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int NUM_OF_WORKING_DAYS = 20;
-            const int MAX_WORKING_HOURS_IN_MONTH = 100;
+            Console.WriteLine("Employee wage: " + ComputeEmployeeWage());
+        }
+        public static int ComputeEmployeeWage()
+        {
             //Variable
-            int employeeHours = 0, totalEmployeeHours = 0, totalWorkingDays = 0;
+            int employeeHours = 0;
+            int totalEmployeeHours = 0;
+            int totalWorkingDays = 0;
 
-            while (totalEmployeeHours <= MAX_WORKING_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmployeeHours < MAX_WORKING_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 //Create reference of random class
                 Random random = new Random();
-                //Returns random value
-                int employeeCheck = random.Next(0, 2);
+                
                 totalWorkingDays++;
 
                 //Switchcase to check fulltime and part time.
-                switch (employeeCheck)
+                switch (random.Next(0, 3))
                 {
                     case IS_EMPLOYEE_FULL_TIME:
                         employeeHours = 8;
@@ -45,11 +50,10 @@ namespace EmployeeWage
                         employeeHours = 0;
                         break;
                 }
-                totalEmployeeHours += employeeHours;                
-                Console.WriteLine("Days: " + totalWorkingDays + "Employee per hour" + employeeHours);
+                totalEmployeeHours += employeeHours;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " +employeeHours);
             }
-            int totalEmployeeWage = employeeHours * EMP_RATE_PER_HOUR * NUM_OF_WORKING_DAYS;
-            Console.WriteLine("Employee wage is: " + totalEmployeeWage);
-        }   
+            return employeeHours * EMP_RATE_PER_HOUR * NUM_OF_WORKING_DAYS;
+        }
     }
 }
