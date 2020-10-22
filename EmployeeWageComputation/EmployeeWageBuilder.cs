76b81f5ddb6876f6562b5e5d5a4f4efb6eb81bca
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-22 17:15:25 +0530

    [Chetan]Add.Save total wage by company

---
+++ EmployeeWageComputation/EmployeeWageBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWageBuilder
+    {
+        //Constants 
+        const int IS_EMPLOYEE_FULL_TIME = 1;
+        const int IS_EMPLOYEE_PART_TIME = 2;
+
+        //Variables
+        private string companyName;
+        private int employeeRatePerHour;
+        private int numberOfWorkingDays;
+        private int maxHrsInMonth;
+        private int totalEmpWage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeWageBuilder"/> class.
+        /// </summary>
+        /// <param name="companyName">Name of the company.</param>
+        /// <param name="employeeRatePerHour">The employee rate per hour.</param>
+        /// <param name="numberOfWorkingDays">The number of working days.</param>
+        /// <param name="maxHrsInMonth">The maximum HRS in month.</param>
+        public EmployeeWageBuilder(string companyName, int employeeRatePerHour, int numberOfWorkingDays, int maxHrsInMonth)
+        {
+            this.companyName = companyName;
+            this.employeeRatePerHour = employeeRatePerHour;
+            this.numberOfWorkingDays = numberOfWorkingDays;
+            this.maxHrsInMonth = maxHrsInMonth;
+        }
+
+        /// <summary>
+        /// Computes the employee wage.
+        /// </summary>
+        /// <returns></returns>
+        public int ComputeEmployeeWage()
+        {
+            //Variable
+            int employeeHours = 0;
+            int totalEmployeeHours = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmployeeHours < maxHrsInMonth && totalWorkingDays < numberOfWorkingDays)
+            {
+                //Create reference of random class
+                Random random = new Random();
+
+                totalWorkingDays++;
+
+                //Switchcase to check fulltime and part time.
+                switch (random.Next(0, 3))
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        employeeHours = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        employeeHours = 4;
+                        break;
+                    default:
+                        employeeHours = 0;
+                        break;
+                }
+                totalEmployeeHours += employeeHours;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " + employeeHours);
+            }
+            //Calculates employee wage and returns it.
+            totalEmpWage= totalEmployeeHours * this.employeeRatePerHour;
+            Console.WriteLine("Total Employee wage for Company: " + companyName + "is: " + totalEmpWage);
+            return 0;//"Total Employee wage for Company: " + companyName + "is: " + totalEmpWage;
+        }
+
+        public string toString()
+        {
+            return "Total Employee wage for Company: " + this.companyName + "is: " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,6 +6,7 @@
  *  @since   21-10-2020
  *
  ******************************************************************************/
+using EmployeeWageComputation;
 using System;
 using System.Reflection.Metadata.Ecma335;
 
@@ -13,54 +14,17 @@ namespace EmployeeWage
 {
     class Program
     {
-        //Constants 
-        const int IS_EMPLOYEE_FULL_TIME = 1;
-        const int IS_EMPLOYEE_PART_TIME = 2;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Employee wage: " + ComputeEmployeeWage("Apple", 50, 20, 100));
-            Console.WriteLine("Employee wage: " + ComputeEmployeeWage("Google", 60, 22, 100));
+            EmployeeWageBuilder apple = new EmployeeWageBuilder("Apple", 50, 20, 100);
+            EmployeeWageBuilder google = new EmployeeWageBuilder("Google", 60, 20, 110);
+            apple.ComputeEmployeeWage();
+            Console.WriteLine(apple.toString());
+            google.ComputeEmployeeWage();
+            Console.WriteLine(google.toString());
         }
-        /// <summary>
-        /// Computes the employee wage.
-        /// </summary>
-        /// <param name="companyName">Name of the company.</param>
-        /// <param name="employeeRatePerHour">The employee rate per hour.</param>
-        /// <param name="numberOfWorkingDays">The number of working days.</param>
-        /// <param name="maxHrsInMonth">The maximum HRS in month.</param>
-        /// <returns></returns>
-        public static int ComputeEmployeeWage(string companyName, int employeeRatePerHour, int numberOfWorkingDays, int maxHrsInMonth)
-        {
-            //Variable
-            int employeeHours = 0;
-            int totalEmployeeHours = 0;
-            int totalWorkingDays = 0;
-
-            while (totalEmployeeHours < maxHrsInMonth && totalWorkingDays < numberOfWorkingDays)
-            {
-                //Create reference of random class
-                Random random = new Random();
-                
-                totalWorkingDays++;
-
-                //Switchcase to check fulltime and part time.
-                switch (random.Next(0, 3))
-                {
-                    case IS_EMPLOYEE_FULL_TIME:
-                        employeeHours = 8;
-                        break;
-                    case IS_EMPLOYEE_PART_TIME:
-                        employeeHours = 4;
-                        break;
-                    default:
-                        employeeHours = 0;
-                        break;
-                }
-                totalEmployeeHours += employeeHours;
-                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " +employeeHours);
-            }
-            //Calculates employee wage and returns it.
-            return employeeHours * employeeRatePerHour * numberOfWorkingDays;
-        }
+        
+        
     }
 }
